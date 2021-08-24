d4d349cd3b4611789fa1f465c3c23bea95589e74
muflikhandimasd <fihan.one@gmail.com>
2021-08-24 20:18:55 +0700

    Nama update

--- src/main/kotlin/Task1IDN.kt
+++ src/main/kotlin/Task1IDN.kt
@@ -1,3 +1,5 @@
+// Nama = Muflikhan Dimas Dwiprayogi
+
 fun main() {
     var name = "Bondan"
     var age = 20
@@ -7,3 +9,9 @@ fun main() {
     age = 18
     println("dan $name adalah mahasiswa baru di politeknik IDN, dia berumur $age tahun")
 }
+
+/*
+Output =
+Bondan adalah mahasiswa baru di politeknik IDN, dia berumur 20 tahun
+dan Agus adalah mahasiswa baru di politeknik IDN, dia berumur 18 tahun
+ */
