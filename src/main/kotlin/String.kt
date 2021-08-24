2a625cb8a24611e6d89bf8532370be9dc38b6e5f
muflikhandimasd <fihan.one@gmail.com>
2021-08-24 20:14:07 +0700

    Initial commit

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,53 @@
+//Tipe data string adalah tipe data yang berisi kumpulan karakter atau teks
+//fun main() {
+//    var modul : String = "Buku"
+//    var kelas : String = """
+//        |saya sekarang ada di IDN
+//        |berkuliah di politeknik IDN
+//    """.trimMargin()
+//    print(kelas)
+//}
+
+//fun main() {
+//    var modul : String = "Buku"
+//    var kelas : String = """
+//        saya sekarang ada di IDN
+//        berkuliah di politeknik IDN
+//    """.trimIndent()
+//    print(kelas)
+//}
+
+//fun main() {
+//    var modul : String = "Buku"
+//    var kelas : String = """
+//        saya sekarang ada di IDN
+//        >berkuliah di politeknik IDN
+//    """.trimMargin(">")
+//    print(kelas)
+//}
+
+//Output =
+//       saya sekarang ada di IDN
+//berkuliah di politeknik IDN
+
+//fun main() {
+//    val firstName = "Muflikhan"
+//    val lastName = "Dimas"
+//
+//    print(firstName + " " + lastName)
+//}
+
+
+//String template
+// GUNAKAN ${(isi ekspresi)}
+
+fun main() {
+    val firstName = "Muflikhan"
+    val lastName = "Dimas"
+    val age = 12
+
+    println("Hello ${firstName.length}")
+
+    //jika $ satu kata ga pake kurawal, jika 2 kata maka pake kurawal, misal ${(name.length)}
+
+}
---
+++ src/main/kotlin/Task1IDN.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var name = "Bondan"
+    var age = 20
+    println("$name adalah mahasiswa baru di politeknik IDN, dia berumur $age tahun")
+
+    name = "Agus"
+    age = 18
+    println("dan $name adalah mahasiswa baru di politeknik IDN, dia berumur $age tahun")
+}
