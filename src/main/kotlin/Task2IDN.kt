e22f5c0d2925757de674fa839c0bc18df000516b
muflikhandimasd <fihan.one@gmail.com>
2021-08-25 22:20:21 +0700

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/kotlin/Task2IDN.kt
@@ -0,0 +1,10 @@
+// Muflikhan Dimas Dwiprayogi
+
+fun main() {
+    val numbers = arrayOf(10, 20, 30, 40)
+    val penjumlahan = numbers[0] + numbers[2]
+    val perkalian = numbers[1] * numbers[3]
+
+    println(penjumlahan)
+    println(perkalian)
+}
